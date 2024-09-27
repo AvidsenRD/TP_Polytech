0deaaf33c45a23799a284fbf791174329b0e66dd
Alrick GIRY <agiry@avidsen.com>
2024-09-27 14:39:07 +0200

    Add new files in build script

--- maison/build_script.sh
+++ maison/build_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-gcc ../mqtt/src/api_mqtt.c ../gpio/src/api_led.c ../gpio/src/api_hall.c ../gpio/src/api_servo.c -l bcm2835 -l paho-mqtt3a -l paho-mqtt3c -o main main.c 
+gcc ../mqtt/src/api_mqtt.c ../gpio/src/api_led.c ../gpio/src/api_hall.c ../gpio/src/api_servo.c ../gpio/src/api_adc.c ../gpio/src/api_thermostat.c -lm -l bcm2835 -l paho-mqtt3a -l paho-mqtt3c -o build/main main.c 
 echo "Build finish"
-sudo ./main
+mkdir build
+sudo ./build/main
