a4c8232af4e1ffbb4d0c8c9b81fbc4a8986101c2
Alrick GIRY <agiry@avidsen.com>
2024-10-01 17:28:27 +0200

    update build script

--- maison/build_script.sh
+++ maison/build_script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-gcc ../mqtt/src/api_mqtt.c ../gpio/src/api_led.c ../gpio/src/api_hall.c ../gpio/src/api_servo.c ../gpio/src/api_adc.c ../gpio/src/api_thermostat.c -lm -l bcm2835 -l paho-mqtt3a -l paho-mqtt3c -o build/main main.c 
+gcc ../mqtt/src/api_mqtt.c ../gpio/src/api_led.c ../gpio/src/api_hall.c ../gpio/src/api_servo.c ../gpio/src/api_adc.c ../gpio/src/api_thermostat.c ../gpio/src/api_button.c  -lm -l bcm2835 -l paho-mqtt3a -l paho-mqtt3c -o build/main main.c 
 echo "Build finish"
-mkdir build
 sudo ./build/main
