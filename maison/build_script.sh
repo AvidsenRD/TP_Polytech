37ec45319f30742f21743fd7c628fe0357b4caf0
AvidsenRD <157726313+AvidsenRD@users.noreply.github.com>
2024-10-02 11:04:02 +0200

    create a build folder

--- maison/build_script.sh
+++ maison/build_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 gcc ../mqtt/src/api_mqtt.c ../gpio/src/api_led.c ../gpio/src/api_hall.c ../gpio/src/api_servo.c ../gpio/src/api_adc.c ../gpio/src/api_thermostat.c ../gpio/src/api_button.c  -lm -l bcm2835 -l paho-mqtt3a -l paho-mqtt3c -o build/main main.c 
+mkdir build
 echo "Build finish"
 sudo ./build/main
